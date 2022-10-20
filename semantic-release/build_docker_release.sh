225575fe95bf1845330f80636f591faee26c2b99
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-10-20 09:05:34 +0200

    semantic release image

--- semantic-release/build_docker_release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if grep docker .releaserc 1>/dev/null; then
-  echo "Docker configuration found in .releaserc"
-  echo -n $CI_REGISTRY_PASSWORD | docker login -u $CI_REGISTRY_USER --password-stdin $CI_REGISTRY
-  docker build --target final -t "$DOCKER_RELEASE_IMAGE" .
-else
-  echo "No docker configuration found in .releaserc. Skipping build"
-fi
